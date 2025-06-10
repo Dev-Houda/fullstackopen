d3ae5c4061b78c7242b6d1ed00ac2dfcc4fcbf1e
DevHouda <devhouda@gmail.com>
2025-06-10 14:51:58 +0100

    The Phonebook step 7 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -36,7 +36,12 @@ const App = () => {
     if (persons.some((e) => e.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(personObject));
+      axios
+        .post("http://localhost:3001/persons", personObject)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+        });
+      //setPersons(persons.concat(personObject));
     }
 
     setNewName("");
