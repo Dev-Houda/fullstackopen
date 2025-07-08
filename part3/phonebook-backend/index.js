cbbe222d533edd5e506be80ce43b0ab9f8391879
Dev-Houda <209240992+Dev-Houda@users.noreply.github.com>
2025-07-08 18:29:12 +0100

    the phonebook backend step 6 complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -62,6 +62,20 @@ const generateId = () => {
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  persons.map((person) => {
+    if (person.name === body.name) {
+      return res.status(400).json({
+        error: "name must be unique",
+      });
+    }
+  });
+
   const person = {
     name: body.name,
     number: body.number,
