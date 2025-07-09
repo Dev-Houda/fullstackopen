bc799bc18fde702fdf999f819844a348864e4653
Dev-Houda <209240992+Dev-Houda@users.noreply.github.com>
2025-07-09 17:55:22 +0100

    Phonebook backend step 8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan(":method :url :status :res[res.data] - :response-time ms"));
 
 let persons = [
   {
